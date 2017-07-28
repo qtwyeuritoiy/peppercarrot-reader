bf4e310f04249989f2becb98ff7ec80b2a200791
Jihoon Kim <imsesaok@tuta.io>
2017-07-29 00:58:41 +0900

    Update license information

--- app/src/main/java/nightlock/peppercarrot/fragments/AboutFragment.kt
+++ app/src/main/java/nightlock/peppercarrot/fragments/AboutFragment.kt
@@ -1,10 +1,46 @@
+/*
+ * Copyright (C) 2017 Jihoon Kim <imsesaok@gmail.com, imsesaok@tuta.io>
+ *
+ * This file is part of Reader for Pepper&Carrot.
+ *
+ * Reader for Pepper&Carrot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package nightlock.peppercarrot.fragments
 
+import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import nightlock.peppercarrot.R
 
 /**
  * Fragment for About screen
  * Created by Jihoon Kim on 4/30/17.
  */
 
-class AboutFragment : Fragment()
+class AboutFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View {
+        val view = inflater!!.inflate(R.layout.fragment_about, container, false)
+        init(view)
+        return view
+    }
+
+    fun init(view: View) {
+        val webView = view.findViewById(R.id.webview_about) as WebView
+        webView.loadUrl("file:///android_asset/about-en_GB.html")
+    }
+}
